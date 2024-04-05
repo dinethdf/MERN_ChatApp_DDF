39eb9187680fbfe12aedf47164086d4ce09aef1c
Dineth Dilshan <dinethdilshan5@gmail.com>
2024-04-05 12:46:07 +0530

    Update static file path in server.js and package-lock.json to Deployment

--- MERN-Chat-App-Backend/server.js
+++ MERN-Chat-App-Backend/server.js
@@ -21,10 +21,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/MERN-Chat-App-Frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(
+    path.join(__dirname, "MERN-Chat-App-Frontend", "dist", "index.html")
+  );
 });
 
 server.listen(PORT, () => {
