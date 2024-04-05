25cd2e6825891737246f88cfa153418198ca4e24
Dineth Dilshan <dinethdilshan5@gmail.com>
2024-04-05 12:59:13 +0530

    Error socket Debug

--- MERN-Chat-App-Frontend/src/context/SocketContext.jsx
+++ MERN-Chat-App-Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:8080", {
+      const socket = io("https://mern-chatapp-ddf.onrender.com/", {
         query: {
           userId: authUser._id,
         },
