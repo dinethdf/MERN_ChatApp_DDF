e97a7c0009984c20292ee98358f49cca006662a2
Dineth Dilshan <dinethdilshan5@gmail.com>
2024-04-12 09:06:13 +0530

    change 2

--- MERN-Chat-App-Frontend/src/pages/signup/SignUp.jsx
+++ MERN-Chat-App-Frontend/src/pages/signup/SignUp.jsx
@@ -37,7 +37,7 @@ const SignUp = () => {
             </label>
             <input
               type="text"
-              placeholder="John Doe"
+              placeholder="Oliver Jake"
               className="w-full input input-bordered  h-10"
               value={inputs.fullName}
               onChange={(e) =>
@@ -52,7 +52,7 @@ const SignUp = () => {
             </label>
             <input
               type="text"
-              placeholder="johndoe"
+              placeholder="oliver2024"
               className="w-full input input-bordered h-10"
               value={inputs.username}
               onChange={(e) =>
